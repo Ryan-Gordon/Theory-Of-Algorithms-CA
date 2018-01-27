a4e5144af892cf0b5dbced55981025096cd50833
Ryan Gordon <ryangordon210@gmail.com>
2018-01-27 12:15:29 +0000

    Finished problem 1

--- problems/01-Primality-Test/primality-test.rkt
+++ problems/01-Primality-Test/primality-test.rkt
@@ -8,6 +8,9 @@
     #t)
   (when (= 3 n)
       #t)
+  (not ; A number is prime if it fails to be divided evenly
+    (for/or [(d (in-range i n))]; Any value that returns true, is not prime
+      (= 0 (remainder n d))))) ; check if n / d is fully dividable
 
 (define (decide-prime n)
   ;If the number is divisible by 2 it cant be a prime other than the edge cases
@@ -15,3 +18,12 @@
       #f
   (if (not (recurFunc n i)) #f #t) ))
 
+; Testcases for the function
+"Testcase 2 - is 2 a prime number?"
+(decide-prime 2)
+"Testcase 3 - is 1 a prime number?"
+(decide-prime 1)
+"Testcase 4 - is 3 a prime number?"
+(decide-prime 3)
+"Testcase 5 - is 7 a prime number?"
+(decide-prime 7)
