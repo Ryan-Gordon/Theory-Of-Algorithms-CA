dcf06f3af6c87e6964f739e26be7ec7cba5f0e42
Ryan Gordon <ryangordon210@gmail.com>
2018-03-15 19:05:44 +0000

    Problem 5 finished

--- problems/05-hamming-weight/05-hamming-weight.rkt
+++ problems/05-hamming-weight/05-hamming-weight.rkt
@@ -1,15 +1,18 @@
 #lang racket
 
-(define weight 0)
-(define (hamming-weight l)
+(define hamming-weight 
   (lambda (l)
     (cond ;ask two questions
-      ((null? l) '()) ;null and else
-      (else (cond
-              (if(positive? (car l) )
-               (+ weight 1) (hamming-weight (cdr l)))
-              (hamming-weight (cdr l))
-               )))))
+      ((null? l) ;The first question
+       0) ;return 0  
+      (else ; else
+       (cond ;ask a question
+         ((= 1 (car l)) ;check if first element is 1
+           (+ 1 (hamming-weight (cdr l)))) ;increment and check again with cdr l
+           (else ; (car l) is not 1
+            (hamming-weight (cdr l))) ; check again with cdr l
+           )))))
 
 
 (hamming-weight (list 1 0 1 0 1 1 1 0)) ; pass list to hamming weight function
+(hamming-weight (list 1 1 1 1 1 1 1 1))
--- problems/05-hamming-weight/README.md
+++ problems/05-hamming-weight/README.md
@@ -5,5 +5,13 @@
 Write a function hamming-weight in Racket that takes a list $l$ as input and returns the number of non-zero elements in it.
 For example:
 > (hamming-weight (list 1 0 1 0 1 1 1 0))
-5
+
+Hamming weight involves recuring over a list of elements and asking 2 questions :
+* if l is null
+* if the car of l is 1
+
+if it is then 1 is added to the weight and the recursion continues
+if not then nothing is added and again, recursion continues.
+
+
 
