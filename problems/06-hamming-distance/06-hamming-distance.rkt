fc67a7a90d950b513ad4a78a4f0b32ed364b1b98
Ryan Gordon <ryangordon210@gmail.com>
2018-03-15 20:06:12 +0000

    Problem 6 finished with cond and recursion

--- problems/06-hamming-distance/06-hamming-distance.rkt
+++ problems/06-hamming-distance/06-hamming-distance.rkt
@@ -1,7 +1,18 @@
 #lang racket
 
 (define hamming-distance
-  
+  (lambda (l m)
+  ;edge case : if either of the lists are null then the hamming-distance is 0
+    (cond
+      ((null? l) (null? m); Ask the first question of a lists  
+                 0 ) ; return 0 if they are null
+      (else ; if the lists arent null
+       (cond ;ask more questions
+         ((= (car l) (car m));if the atom car l is equal to the atom car m:
+          (hamming-distance (cdr l) (cdr m))); dont increment, continue recursion
+         (else ;if the (car l) (car m) IS equal
+          (+ 1 (hamming-distance (cdr l) (cdr m)))))
+       )))); increment distance and continue recursion
         
 
 (hamming-distance (list 1 1 1 1 0 0 0 0) (list  1 0 1 0 1 1 1 0))
