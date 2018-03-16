acc0a630d85cefc831553ea17691c86bde86b590
Ryan Gordon <ryangordon210@gmail.com>
2018-03-16 20:09:00 +0000

    problem 7 and readme

--- problems/07-maj/07-maj.rkt
+++ problems/07-maj/07-maj.rkt
@@ -4,5 +4,12 @@
   (cond
     ((null? x) (null? y) (null? z) ;ask if lists are null 
      '() ) ;if so return the null list
-    
+    (else (map ;else start a map
+           (lambda (a b c) ; lambda taking 3 args, each an instance of x,y,z
+             (cond ; ask 2 questions, cond and else
+               ((>= (+ a b c) 2) 1) ;if sum of an instance of a,b,c is >=2 return 1
+               (else 0))) ; else return 0 meaning 1 or less is of the sum
+           x y z)))) ; map x -> a, y -> b, and z -> c
+
+
 (maj (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
---
+++ problems/07-maj/README.md
@@ -0,0 +1,9 @@
+# Theory-Of-Algorithms-CA
+# Problem 7 : Maj
+
+
+Write a function maj in racket that takes three lists x,y and z of equal length and containing only 0's and 1's. It should return a list containing 1 where two or more of the x y or z values are 1,  and 0 otherwise
+For example:
+>(maj (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
+'(0 0 0 1 0 1 1 1)
+
